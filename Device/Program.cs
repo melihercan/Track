f9411a8af3d6e8b292fb986eb3054463a1e205f4
Melih Ercan <melihercan@gmail.com>
2019-12-21 19:47:20 +0100

    Device updates.

--- Device/Program.cs
+++ Device/Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Shared.Services;
+using System;
+using System.Threading.Tasks;
 
 namespace Device
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new HostBuilder()
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile("appsettings.json", optional: true);
+                    config.AddEnvironmentVariables();
+
+                    if (args != null)
+                    {
+                        config.AddCommandLine(args);
+                    }
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddOptions();
+                    ///services.Configure<AppConfig>(hostContext.Configuration.GetSection("AppConfig"));
+
+                    ///services.AddSingleton<IHostedService, DeviceDataService>();
+                    services.AddSingleton<IHubConnectionBuilder, HubConnectionBuilder>();
+                    services.AddHostedService<DeviceDataService>();
+                })
+                .ConfigureLogging((hostingContext, logging) => {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                });
+
+            await builder.RunConsoleAsync();
         }
     }
 }
