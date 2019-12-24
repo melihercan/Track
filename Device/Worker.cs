7525de7588211859ed490f1582848a18cd0d7a23
Melih Ercan <melihercan@gmail.com>
2019-12-24 19:05:06 +0100

    Handling connection operations on Device side.

--- Device/Worker.cs
+++ Device/Worker.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -10,15 +13,16 @@ namespace Device
 {
     public class Worker : BackgroundService
     {
+        private readonly ILogger<Worker> _logger;
         private readonly int _numDevices = 5;
         private readonly int _delay = 1000;
-        private readonly ILogger<Worker> _logger;
 
 
 
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
+
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -36,10 +40,56 @@ namespace Device
 
         private async Task DoWorkAsync(int id, CancellationToken ct)
         {
+            var connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:5001/DeviceDataIn")
+////                .WithAutomaticReconnect()
+                .AddMessagePackProtocol()
+                .Build();
+
+            connection.Closed += ex =>
+            {
+                // Start connection again without awaiting.
+                _ = ConnectWithRetryAsync(connection, ct);
+                return Task.CompletedTask;
+            };
+
+            // Await till connection is made or cancel occured.
+            await ConnectWithRetryAsync(connection, ct);
+
             while (!ct.IsCancellationRequested)
             {
-                _logger.LogInformation($"Device {id} running at {DateTimeOffset.Now}");
+                if (connection.State == HubConnectionState.Connected)
+                {
+
+                    _logger.LogInformation($"Device {id} running at {DateTime.Now}");
+                }
                 await Task.Delay(_delay, ct);
+            }
+        }
+
+        private async Task<bool> ConnectWithRetryAsync(HubConnection connection, CancellationToken ct)
+        {
+            // Keep trying until we can start or the token is canceled.
+            while (true)
+            {
+                try
+                {
+                    await connection.StartAsync(ct);
+                    Debug.Assert(connection.State == HubConnectionState.Connected);
+                    _logger.LogInformation($"Connected to hub with ConnectionId: {connection.ConnectionId}");
+                    return true;
+                }
+                catch when (ct.IsCancellationRequested)
+                {
+                    return false;
+                }
+                catch
+                {
+                    // Failed to connect, trying again in 5000 ms.
+                    Debug.Assert(connection.State == HubConnectionState.Disconnected);
+                    _logger.LogInformation($"Will try to connect to hub again after 5s...");
+                    await Task.Delay(5000);
+                }
             }
         }
     }
